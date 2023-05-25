05b2efc8ee5da4e2cd5e1febfdd9685ec7709070
Manik Hasan <107718201+manik202@users.noreply.github.com>
2023-05-26 00:50:35 +0600

    html css react redux-toolkit crud app-2023

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+import Index from './routes';
+
+const App = () => {
+  return (
+    <div className='App'>
+   <Index />
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/layouts/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+        <p>Copyright by Manik Hasan @2023</p>
+    </footer>
+  )
+}
+
+export default footer
